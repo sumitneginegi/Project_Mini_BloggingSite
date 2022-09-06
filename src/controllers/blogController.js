9cf300d61878d21263e9b3200e36ac87ab41f4dc
NehaVerma009 <nehuverma009@gmail.com>
2022-09-06 12:06:12 +0530

    project1-Day2

--- src/controllers/blogController.js
+++ src/controllers/blogController.js
@@ -1,19 +1,36 @@
-const blog = require("../models/blogsModel")
-
+const blog = require("../models/blogModel")
+const authorModel = require("../models/authorModel")
+//const time =  require("log-timestamp")
 
 
 const createBlog= async function (req, res) {
     try{
     let Blog = req.body
-    let authorId= req.body
+    let published= req.body.published
+   
+
+    if(!Blog.authorId){
+        res.status(400).send({msg:"AuthorId is not present"})
+    }
+    let authorId = await authorModel.findById({_id:Blog.authorId})
     if(!authorId){
         res.status(400).send({msg:"AuthorId is Invalid"})
     }
+    
+    // if(published== true){
+    //    console.log({publishedAt:Date()})
+    //  }else res.send({msg:"Blog not published"})
+
+    //  if(Blog.Deleted== true){
+    //     console.log({deletedAt:Date()})
+    //   }else res.send({msg:"Blog is not Deleted"})
+     
     let BlogCreated = await blog.create(Blog)
     res.status(201).send({data: BlogCreated})
     if(!Blog){
         res.status(404).send({msg:"This is Invalid Request",status:false})
     }
+    
 
 }
 catch(err){
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,22 +1,18 @@
 const express = require('express');
 const router = express.Router();
 
-// const authorController= require("../controllers/authorController")
+ const authorController= require("../controllers/authorController")
 // const bookController= require("../controllers/bookController")
 const blogController= require("../controllers/blogController")
 
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
+
 router.post("/createblog",blogController.createBlog)
 
 
-
-
-
-
-
-// router.post("/createAuthor", authorController.createAuthor  )
+ router.post("/createAuthor", authorController.createAuthor  )
 
 // router.get("/getAuthorsData", authorController.getAuthorsData)
 
