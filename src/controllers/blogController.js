662ce072f6c9197727373dc45588759a433aa334
NehaVerma009 <nehuverma009@gmail.com>
2022-09-06 16:59:55 +0530

    updatedBlog

--- src/controllers/blogController.js
+++ src/controllers/blogController.js
@@ -1,22 +1,19 @@
-const blog = require("../models/blogModel")
-const authorModel = require("../models/authorModel")
-//const time =  require("log-timestamp")
+const blog = require("../models/blogModel");
+const authorModel = require("../models/authorModel");
 
+const createBlog = async function (req, res) {
+  try {
+    let Blog = req.body;
+    let published = req.body.published;
 
-const createBlog= async function (req, res) {
-    try{
-    let Blog = req.body
-    let published= req.body.published
-   
-
-    if(!Blog.authorId){
-        res.status(400).send({msg:"AuthorId is not present"})
+    if (!Blog.authorId) {
+      res.status(400).send({ msg: "AuthorId is not present" });
     }
-    let authorId = await authorModel.findById({_id:Blog.authorId})
-    if(!authorId){
-        res.status(400).send({msg:"AuthorId is Invalid"})
+    let authorId = await authorModel.findById({ _id: Blog.authorId });
+    if (!authorId) {
+      res.status(400).send({ msg: "AuthorId is Invalid" });
     }
-    
+
     // if(published== true){
     //    console.log({publishedAt:Date()})
     //  }else res.send({msg:"Blog not published"})
@@ -24,20 +21,51 @@ const createBlog= async function (req, res) {
     //  if(Blog.Deleted== true){
     //     console.log({deletedAt:Date()})
     //   }else res.send({msg:"Blog is not Deleted"})
-     
-    let BlogCreated = await blog.create(Blog)
-    res.status(201).send({data: BlogCreated})
-    if(!Blog){
-        res.status(404).send({msg:"This is Invalid Request",status:false})
+
+    let BlogCreated = await blog.create(Blog);
+    res.status(201).send({ data: BlogCreated });
+    if (!Blog) {
+      res.status(404).send({ msg: "This is Invalid Request", status: false });
     }
-    
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send({ msg: err.message });
+  }
+};
+//=====================UpdateBlog========================================
+
+const updatedBlog = async function (req, res) {
+    try{
+  let updateId = req.params.blogId;
+  let blogId = await blog.findById({ _id:updateId});
+    if (!blogId) {
+      res.status(404).send({ status:false,msg: "blogId is Invalid" });
+    }
+    //else res.status(200).send({status:true,data:blogId})
+
+
+  let updateBlog = await blog.findOneAndUpdate(
+    { _id: updateId }, //condition
+    { $set: { title: "FunActivities"} }, //update
+    { new: true }) //return updated value
+    res.status(200).send({data:updateBlog})
+ 
+  
+
+  if (published == true) {
+    console.log({ publishedAt: Date() });
+  } else res.status(400).send({ msg: "Blog not published" });
+
 
 }
+
 catch(err){
     console.log(err.message)
     res.status(500).send({msg:err.message})
-
-}
 }
 
-module.exports.createBlog= createBlog
+}
+
+
+module.exports.createBlog = createBlog;
+module.exports.updatedBlog = updatedBlog;
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,25 +1,33 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
- const authorController= require("../controllers/authorController")
-// const bookController= require("../controllers/bookController")
-const blogController= require("../controllers/blogController")
+const authorController = require("../controllers/authorController");
+const blogController = require("../controllers/blogController");
+
+
+
 
 router.get("/test-me", function (req, res) {
-    res.send("My first ever api!")
-})
-
-router.post("/createblog",blogController.createBlog)
+  res.send("My first ever api!");
+});
 
 
- router.post("/createAuthor", authorController.createAuthor  )
+router.post("/createAuthor", authorController.createAuthor);
+router.post("/createblog", blogController.createBlog);
+
+router.put("/blogs/:blogId",blogController.updatedBlog)
+
+
+
+
+
+
+
+
 
 // router.get("/getAuthorsData", authorController.getAuthorsData)
-
 // router.post("/createBlog", bookController.createBlog  )
-
 // router.get("/getBooksData", bookController.getBooksData)
-
 // router.get("/getBooksWithAuthorDetails", bookController.getBooksWithAuthorDetails)
 
 module.exports = router;
